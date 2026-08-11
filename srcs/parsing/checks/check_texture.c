cf41c57309b747923634ef33ef77655d3a4db7f0
biraj-phuyal <birajphuyal11@gmail.com>
2026-08-11 16:58:05 +0100

    Validating caps has already been done while checking the invalid and duplicates so I have removed it

--- srcs/parsing/checks/check_texture.c
+++ srcs/parsing/checks/check_texture.c
@@ -6,7 +6,7 @@
 /*   By: biphuyal <biphuyal@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/22 18:22:12 by biphuyal          #+#    #+#             */
-/*   Updated: 2026/08/09 17:43:44 by biphuyal         ###   ########.fr       */
+/*   Updated: 2026/08/11 16:57:23 by biphuyal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,6 @@ bool	check_texture(t_map_vars map_vars)
 	if (!valid_spacing(map_vars))
 		return(false);
 	if (!valid_permisions(map_vars))
-		return(false);
-	if (!valid_CAPS(map_vars))
 		return(false);
 	return (true);
 }
